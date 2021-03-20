b660dc12ff255e2039c48e2188461d02e38b4a93
Alexander Kell <alexander@kell.es>
2021-03-20 19:06:38 +0000

    adding to model

--- src/models/battery_model.py
+++ src/models/battery_model.py
@@ -6,6 +6,11 @@ class Battery:
         self.battery_size = battery_size
         self.current_charge = 0
 
+    def use_battery(energy):
+        if energy > 0:
+            self.charge(energy)
+        elif energy < 0:
+            self.discharge(energy)
 
     def charge(charge_size):
         if self.current_charge + charge_size < self.battery_size:
@@ -15,27 +20,26 @@ class Battery:
             residual_energy = self.battery_size - self.current_charge
             self.current_charge = self.battery_size
             return residual_energy
-            
-
+    
     def discharge(discharge_size):
         if self.current_charge >= discharge_size:
             self.current_charge -= discharge_size
             return True
         else:
-            return False
+            return 90000
 
     def is_battery_full():
         if self.current_charge == self.battery_size:
             return True
         else:
-            return False
+            return 90000
 
 
 class HouseSystem:
 
     time_step = timedelta(minutes=30)
-
-    def __init__(battery_size, customer_number, generator_capacity, postcode, solar_generation, controlled_load_consumption, general_electricity_consumption):
+    # tariff data: https://www.canstarblue.com.au/electricity/controlled-load-tariff-can-save-money/
+    def __init__(battery_size, customer_number, generator_capacity, postcode, solar_generation, controlled_load_consumption, general_electricity_consumption, single_rate_tariff=27, controlled_load_tariff=10):
         self.battery_size = battery_size
         self.battery = Battery(battery_size)
         self.customer_number = customer_number
@@ -44,21 +48,30 @@ class HouseSystem:
         self.solar_generation = solar_generation
         self.controlled_load_consumption = controlled_load_consumption
         self.general_electricity_consumption
+        self.single_rate_tarrif = single_rate_tarrif
+        self.controlled_load_tariff = controlled_load_tariff
         self.datetime = datetime(2012, 1, 8, 0, 30)
     
-    def step():
+    def step(charge_action):
         self.datetime += time_step
         current_solar = self.solar_generation[self.solar_generation.datetime == self.datetime]
         current_controlled_load_consumption = self.controlled_load_consumption[self.controlled_load_consumption.datetime == self.datetime]
         current_general_electricity_consumption = self.general_electricity_consumption[self.general_electricity_consumption.datetime == self.datetime]
 
-        if current_solar > 0 and self.battery.is_battery_full() is False:
-            self.battery.charge(current_solar)
+        self.battery.use_battery(charge_action)
+
+    def electricity_cost(general_electricity_consumption, controlled_load_consumption)
+        general_consumption_cost = self.single_rate_tariff * general_electricity_consumption
+        controlled_consumption_cost = self.controlled_load_tariff * controlled_load_consumption
+
+        step_cost = general_consumption_cost + controlled_consumption_cost
+        return step_cost
 
 
 
 
-        
+
+
 
 
 
---
+++ src/models/run_model.py
@@ -0,0 +1,12 @@
+import gym, ray
+from ray.rllib.agents import ddpg
+import HouseSystem
+
+class MyEnv(gym.Env):
+    def __init__(self, env_config):
+        self.action_space = <gym.Space>
+        self.observation_space = <gym.Space>
+    def reset(self):
+        return <obs>
+    def step(self, action):
+        return <obs>, <reward: float>, <done: bool>, <info: dict>
