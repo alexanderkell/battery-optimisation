31eec222cc0d8254206b9dc4bdc362352279355f
Alexander Kell <alexander@kell.es>
2021-03-21 12:45:59 +0000

    adding tests to house system factory

--- src/models/battery_model.py
+++ src/models/battery_model.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+import pandas as pd
+
 
 class Battery:
 
@@ -22,7 +24,7 @@ class Battery:
             residual_energy = self.battery_size - self.current_charge
             self.current_charge = self.battery_size
             return residual_energy
-    
+
     def discharge(self, discharge_size):
         if self.current_charge >= - discharge_size:
             self.current_charge += discharge_size
@@ -41,9 +43,38 @@ class Battery:
             return 90000
 
 
-class HouseSystem:
+class HouseSystemFactory():
+    def __init__(self, battery_size):
+        self.battery_size = battery_size
 
-    time_step = timedelta(minutes=30)
+    def create_house_system(self, input, single_rate_tariff=27, controlled_load_tariff=10):
+        if isinstance(input, pd.DataFrame):
+
+            house_system_list = []
+            for (customer_number, generator_capacity, postcode), data in input.groupby(["Customer", "Generator Capacity", "Postcode"]):
+                solar_generation = data[data['Consumption Category']
+                                        == "solar_generation"]
+                controlled_load_consumption = data[data['Consumption Category']
+                                                   == "controlled_load_consumption"]
+                general_electricity_consumption = data[data['Consumption Category']
+                                                       == "general_electricity_consumption"]
+
+                house_system = HouseSystem(
+                    self.battery_size,
+                    customer_number,
+                    generator_capacity,
+                    postcode,
+                    solar_generation,
+                    controlled_load_consumption,
+                    general_electricity_consumption,
+                    single_rate_tariff,
+                    controlled_load_tariff
+                )
+                house_system_list.append(house_system)
+            return house_system_list
+
+
+class HouseSystem:
     # tariff data: https://www.canstarblue.com.au/electricity/controlled-load-tariff-can-save-money/
     def __init__(self, battery_size, customer_number, generator_capacity, postcode, solar_generation, controlled_load_consumption, general_electricity_consumption, single_rate_tariff=27, controlled_load_tariff=10):
         self.battery_size = battery_size
@@ -53,48 +84,51 @@ class HouseSystem:
         self.postcode = postcode
         self.solar_generation = solar_generation
         self.controlled_load_consumption = controlled_load_consumption
-        self.general_electricity_consumption
-        self.single_rate_tarrif = single_rate_tarrif
+        self.general_electricity_consumption = general_electricity_consumption
+        self.single_rate_tarrif = single_rate_tariff
         self.controlled_load_tariff = controlled_load_tariff
         self.datetime = datetime(2012, 1, 8, 0, 30)
-    
+
+        self.time_step = timedelta(minutes=30)
+
     def step(self, charge_action):
-        self.datetime += time_step
+        self.datetime += self.time_step
         current_solar = self.solar_generation[self.solar_generation.datetime == self.datetime]
-        current_controlled_load_consumption = self.controlled_load_consumption[self.controlled_load_consumption.datetime == self.datetime]
-        current_general_electricity_consumption = self.general_electricity_consumption[self.general_electricity_consumption.datetime == self.datetime]
+        current_controlled_load_consumption = self.controlled_load_consumption[
+            self.controlled_load_consumption.datetime == self.datetime]
+        current_general_electricity_consumption = self.general_electricity_consumption[
+            self.general_electricity_consumption.datetime == self.datetime]
 
         residual_battery_energy = self.battery.use_battery(charge_action)
 
-        if residual_battery_energy > 0:    
-            if current_controlled_load_consumption > 0:
-                residual_controlled_load_consumption = current_controlled_load_consumption - residual_battery_energy
-            if current_general_electricity_consumption > 0:
-                residual_general_electricity_consumption = current_general_electricity_consumption - residual_battery_energy
+        residual_general_electricity_consumption, residual_controlled_load_consumption = self.service_electricity_load(
+            residual_battery_energy, current_controlled_load_consumption, current_general_electricity_consumption)
 
-        cost = self.electricity_cost(residual_general_electricity_consumption, residual_controlled_load_consumption)
+        cost = self.electricity_cost(
+            residual_general_electricity_consumption, residual_controlled_load_consumption)
         reward = -cost
-        done = True if self.datetime == datetime(2013, 12, 6, 23, 30) else False
+        done = True if self.datetime == datetime(
+            2013, 12, 6, 23, 30) else False
 
-        observations = [self.battery.battery_size, self.battery.current_charge, residual_general_electricity_consumption, residual_controlled_load_consumption]
+        observations = [self.battery.battery_size, self.battery.current_charge,
+                        residual_general_electricity_consumption, residual_controlled_load_consumption]
 
         return observations, reward, done
 
-
+    def service_electricity_load(self, residual_battery_energy, current_controlled_load_consumption, current_general_electricity_consumption):
+        if residual_battery_energy > 0:
+            if current_controlled_load_consumption > 0:
+                residual_controlled_load_consumption = current_controlled_load_consumption - \
+                    residual_battery_energy
+            if current_general_electricity_consumption > 0:
+                residual_general_electricity_consumption = current_general_electricity_consumption - \
+                    residual_battery_energy
+        return residual_general_electricity_consumption, residual_controlled_load_consumption
 
     def electricity_cost(self, general_electricity_consumption, controlled_load_consumption):
         general_consumption_cost = self.single_rate_tariff * general_electricity_consumption
-        controlled_consumption_cost = self.controlled_load_tariff * controlled_load_consumption
+        controlled_consumption_cost = self.controlled_load_tariff * \
+            controlled_load_consumption
 
         step_cost = general_consumption_cost + controlled_consumption_cost
         return step_cost
-
-
-
-
-
-
-
-
-
-
--- test/test_model.py
+++ test/test_model.py
@@ -1,10 +1,13 @@
 from pytest import fixture
+import pandas as pd
+from pathlib import Path
 
 @fixture
 def model():
     from src.models.battery_model import Battery
 
     return Battery(5000)
+
 
 def test_charge_battery(model):
     model.use_battery(10)
@@ -18,7 +21,36 @@ def test_charge_battery(model):
     model.use_battery(model.battery_size + 500)
     assert model.current_charge == model.battery_size
 
+
 def test_charge_battery_reward(model):
     assert model.use_battery(5) == 0
     assert model.use_battery(-10) == 5
     assert model.use_battery(model.battery_size + 5000) == 5000
+
+
+@fixture
+def house_system_list():
+    from src.models.battery_model import HouseSystemFactory
+
+    project_dir = Path(__file__).resolve().parents[1]
+    consumption_data_path = "{}/data/processed/lagged_2012-2013-solar-electricity-data.csv".format(
+        project_dir)
+
+    consumption_data = pd.read_csv(
+        consumption_data_path,
+        nrows=20000
+    )
+
+    factory = HouseSystemFactory(battery_size=5000)
+    house_system_list = factory.create_house_system(consumption_data)
+
+    return house_system_list
+
+
+def test_house_system_factory(house_system_list):
+    house_system = house_system_list[0]
+
+    assert house_system.customer_number == 1
+    assert house_system.generator_capacity == 3.78
+    assert house_system.postcode == 2076
+    
